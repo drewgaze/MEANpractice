1b64de0bdf12cb6017beded29f253ebfa1549fcc
drewgaze <adrmcgarrity@gmail.com>
2015-12-21 18:25:12 -0500

    fixed typos

--- ng/applicationctrl.js
+++ ng/applicationctrl.js
@@ -1,7 +1,7 @@
 angular.module('app')
 .controller('ApplicationCtrl', function($scope) {
 
-	$scope.on('login', function(_, user) {
+	$scope.$on('login', function(_, user) {
 
 		$scope.currentUser = user;
 	});
