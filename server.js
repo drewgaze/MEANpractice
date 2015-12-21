fd6bfdbfde3e458cfebb22207b4edf814c1acbd3
drewgaze <adrmcgarrity@gmail.com>
2015-12-21 18:39:55 -0500

    auth middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(require('./auth'));
 app.use(require('./controllers'));
 app.use('/api/sessions', require('./controllers/api/sessions'));
 app.use('/api/users', require('./controllers/api/users'));
