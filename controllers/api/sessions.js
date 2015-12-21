f3198ab4c17c57d66a2309cb581c15277701ded1
drewgaze <adrmcgarrity@gmail.com>
2015-12-21 18:13:42 -0500

    added templates

---
+++ controllers/api/sessions.js
@@ -0,0 +1,27 @@
+var router = require('express').Router();
+var User = require('../../models/user');
+var bcrypt = require('bcrypt');
+var jwt = require('jwt-simple');
+var config = require('../../config');
+
+router.post('/', function(req, res, next) {
+
+	User.findOne({username: req.body.username})
+	.select('password').select('username')
+	.exec(function(err, user) {
+
+		if (err) return next(err);
+		if (!user) res.sendStatus(401);
+
+		bcrypt.compare(req.body.password, user.password, function(err, valid) {
+
+			if (err) return next(err);
+			if (!valid) return res.sendStatus(401);
+
+			var token = jwt.encode({username: user.password}, config.secret);
+			res.send(token);
+		});
+	});
+});
+
+module.exports = router;
---
+++ ng/loginctrl..js
@@ -0,0 +1,12 @@
+angular.module('app')
+.controller('LoginCtrl', function($scope, UserSvc) {
+
+	$scope.login = function(username, password) {
+
+		UserSvc.login(username, password)
+		.then(function(user) {
+
+			console.log(user);
+		});
+	}
+});
